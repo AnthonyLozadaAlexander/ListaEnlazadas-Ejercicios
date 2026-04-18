4f0f998f9d924257b5066420216f296c08bd1527
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 02:06:20 -0500

    Agrega método main para probar la clase ListaEnlazada
    Se implementa un método main en la clase Main que permite
    probar las funcionalidades de la clase ListaEnlazada.
    Se agregan ejemplos de inserción de nodos al inicio y al final,
    así como la impresión del estado de la lista después de cada operación.

--- src/E1/Main.java
+++ src/E1/Main.java
@@ -1,4 +1,19 @@
-﻿package E1;
+package E1;
 
 public class Main {
+    public static void main(String[] args) {
+        ListaEnlazada lista = new ListaEnlazada();
+
+        System.out.println("Agregamos 10 al inicio: ");
+        lista.insertarAlInicio(10);
+        lista.imprimir();
+
+        System.out.println("\nAgregando 20 al final: ");
+        lista.insertarAlFinal(20);
+        lista.imprimir();
+
+        System.out.println("\nAgregando 5 al inicio: ");
+        lista.insertarAlInicio(5);
+        lista.imprimir();
+    }
 }
