37a9be0b711da897f0072c1f6cdc9d775d384111
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 01:52:57 -0500

    Agrega clase ListaEnlazada con método para insertar nodos
    Se implementa la clase ListaEnlazada que permite crear una lista enlazada simple.
    Incluye un método para insertar nodos al inicio de la lista.
    Esta funcionalidad es fundamental para la manipulación de estructuras de datos en el proyecto.

---
+++ src/E1/ListaEnlazada.java
@@ -0,0 +1,28 @@
+﻿package E1;
+
+public class ListaEnlazada {
+
+    private class Nodo { // clase encapsulada para el Nodo
+        int data;
+        Nodo next;
+
+        public Nodo(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Puntero maestro: nos dice donde empieza la lista
+    private Nodo cabeza;
+
+    // Constructor: Al crear la lista, la cabeza es null
+    public ListaEnlazada(){
+        this.cabeza = null;
+    }
+
+    public void insertarAlInicio(int data){
+        Nodo nuevoNodo = new Nodo(data);
+        nuevoNodo.next = cabeza;
+        cabeza = nuevoNodo;
+    }
+}
